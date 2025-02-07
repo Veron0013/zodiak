05ba4a36dfcbf066411eb906ab1ebea355a843ed
Mr.Igor <iv_mirsoft@ukr.net>
2025-02-07 19:20:54 +0200

    js

--- java/script.js
+++ java/script.js
@@ -5,13 +5,27 @@ function loadText(buttonId) {
 	fetch('data.json')
 		.then(response => response.json()) // перетворюємо відповідь на JSON
 		.then(buttonTexts => {
+			if (buttonTexts[id] == buttonId) {
+				console.log(buttonTexts);
+
+				buttonTexts.forEach(val => {
+					console.log(val);
+
+					try {
+						document.getElementById("paragraph").textContent = val;
+					} catch (error) {
+
+					}
+				});
+			}
 			const paragraph = document.getElementById("paragraph");
 			paragraph.textContent = buttonTexts[buttonId]; // Виводимо текст відповідно до id кнопки
 		})
 		.catch(error => console.error('Error loading JSON file:', error));
 }
 
+loadText(1);
 // Прив'язуємо обробники подій до кнопок
-document.getElementById("btn1").addEventListener("click", () => loadText("btn1"));
-document.getElementById("btn2").addEventListener("click", () => loadText("btn2"));
-document.getElementById("btn3").addEventListener("click", () => loadText("btn3"));
+//document.getElementById("btn1").addEventListener("click", () => loadText("btn1"));
+//document.getElementById("btn2").addEventListener("click", () => loadText("btn2"));
+//document.getElementById("btn3").addEventListener("click", () => loadText("btn3"));
